f5c73f725a913b567381e36abf557c229211e3e5
tensliz <89417234+tensliz@users.noreply.github.com>
2022-04-14 17:46:56 -0600

    Revert "updated js file"
    This reverts commit 448da4aaf871d8c31ad1f8b16903b917d4344ddb.

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-//function calcTip() {
- // var billAmount = document.getElementById('bill-amount').value;
- //var tipPercent = document.getElementById('tip-percent').value;
- //var tipAmount = document.getElementById('tip-amount').value;
-
- //var tipAmount = billAmount*tipPercent;
-//}
-
-//button.addEventListener('click', function() {
-//tipAmount();
-//})
-
 /**
  * Passing data to functions through parameters.
  * @link https://developer.mozilla.org/en-US/docs/Glossary/Function
@@ -18,18 +6,27 @@
  * List of ISO language codes:
  * @link http://www.lingoes.net/en/translator/langcode.htm
  */
- const formatter = (locale = "en-US", currency = "USD", value) => {
-  let formattedValue = new Intl.NumberFormat(locale, {
-    style: "currency",
-    currency: currency,
-  }).format(value);
- 
-  return formattedValue;
-};
 
- 
-button.addEventListener('click', function(){
-let billAmount = document.getElementById('bill-amount').value;
-let tipPercent = document.getElementById('tip-percent').value;
-let tipAmount = document.getElementById('tip-amount').value = billAmount * tipPercent;
-})
+ const formatter = (locale = "en-US", currency = "USD", value) => {
+    let formattedValue = new Intl.NumberFormat(locale, {
+      style: "currency",
+      currency: currency,
+    }).format(value);
+  
+    return formattedValue;
+  };
+  
+  const tipCalculator = (sum, percentage, locale, currency) => {
+    let tip = sum * (percentage / 100);
+    let total = sum + tip;
+  
+    console.log(`
+      Sum before tip: ${formatter(locale, currency, sum)}
+      Tip percentage: ${percentage}%
+      Tip:            ${formatter(locale, currency, tip)}
+      Total:          ${formatter(locale, currency, total)}
+    `);
+  };
+  
+  tipCalculator(29.95, 18, "en", "USD");
+  
